fa8da4ea39b97d68f4e8a98ffae0d4e5a58a693f
jazal <jazalahamed19@gmail.com>
2022-06-07 19:05:32 +0530

    Adding EF Core 5 and added product and category

---
+++ Core/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
